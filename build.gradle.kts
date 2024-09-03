2ea5c80f2cb871846231276a4dc74d2e79848dd9
Luis Ricardo Alves Santos <luisricardo2825@gmail.com>
2024-09-03 14:03:01 -0300

    Alteração do usuario e token

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ publishing {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/Luisricardo2825/UtilsSNK")
             credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                username = System.getenv("USERNAME")
+                password = System.getenv("TOKEN")
             }
         }
     }
